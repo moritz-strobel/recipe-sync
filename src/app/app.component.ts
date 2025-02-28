e5101df0a5161e82708b53997767a075a62adf33
Felix <96548144+CoolerFe05@users.noreply.github.com>
2025-02-28 22:45:53 +0100

    revert all changes for new structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, FooterComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
