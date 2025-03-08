659bbe9d07eca7283fc2ce5b75026205182a3d69
Felix <96548144+CoolerFe05@users.noreply.github.com>
2025-03-08 14:49:21 +0100

    code cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { AboutUsComponent } from './page/about-us/about-us.component';
 import { CookbookComponent } from './page/cookbook/cookbook.component';
 import { CookbookHomeComponent } from './page/cookbook-home/cookbook-home.component';
-import { ExtendedSearchComponent } from './page/extended-search/extended-search.component';
 import { GuidelinesComponent } from './page/guidelines/guidelines.component';
 import { ImpressComponent } from './page/impress/impress.component';
 import { LandingComponent } from './page/landing/landing.component';
@@ -12,13 +11,13 @@ import { ReportComponent } from './page/report/report.component';
 import { LoginComponent } from './page/login/login.component';
 import { RegisterComponent } from './page/register/register.component';
 import { ForgotPasswordComponent } from './page/forgot-password/forgot-password.component';
+import { SearchResultContainerComponent } from './page/search-result-container/search-result-container.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent },
     { path: 'about-us', component: AboutUsComponent },
     { path: 'cookbook', component: CookbookComponent },
     { path: 'cookbook-home', component: CookbookHomeComponent },
-    { path: 'extended-search', component: ExtendedSearchComponent },
     { path: 'guidelines', component: GuidelinesComponent },
     { path: 'impress', component: ImpressComponent },
     { path: 'login', component: LoginComponent},
@@ -27,5 +26,59 @@ export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'forgot-password', component: ForgotPasswordComponent },
     { path: 'report', component: ReportComponent },
+    { path: 'search', component: SearchResultContainerComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
+
+
+/*
+To match a path in Angular that contains a query parameter pattern like "?search=", you can use Angular's route parameters and query parameters. Here's how you can define a route that matches when the URL contains the search query parameter:
+
+typescript
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchResultContainerComponent } from './search-result-container/search-result-container.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SearchResultContainerComponent,
+    // Route configuration for query parameters
+    runGuardsAndResolvers: 'paramsOrQueryParamsChange'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+And in your SearchResultContainerComponent, you can check for the presence of the search query parameter:
+
+typescript
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-search-result-container',
+  templateUrl: './search-result-container.component.html',
+  styleUrls: ['./search-result-container.component.css']
+})
+export class SearchResultContainerComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const search = params['search'];
+      if (search) {
+        // Logic to display search results
+        console.log('Search query:', search);
+      }
+    });
+  }
+}
+This way, the SearchResultContainerComponent will be displayed whenever the URL contains the search query parameter, and you can handle the logic within the component accordingly.
+
+Is there anything else I can assist you with? Maybe some more Angular tips or another topic you're working on?
+*/
