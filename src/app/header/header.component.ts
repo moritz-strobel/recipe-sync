b9f8b463927da1bb4dd7b7516551ef09560d7835
Felix <96548144+CoolerFe05@users.noreply.github.com>
2025-03-10 13:40:18 +0100

    finish searchbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SearchbarComponent } from "../searchbar/searchbar.component";
 
 @Component({
   selector: 'app-header',
-  imports: [ ],
+  imports: [SearchbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
