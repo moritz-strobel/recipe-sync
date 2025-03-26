ba8f5610e917a05fdde3f8e056e919491dcb344b
Felix <96548144+CoolerFe05@users.noreply.github.com>
2025-03-26 21:30:59 +0100

    fix styling of profile page

--- src/app/card-cookbook/card-cookbook.component.ts
+++ src/app/card-cookbook/card-cookbook.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CookBook } from '../custom-types/cookbook.type';
 import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-card-cookbook',
   imports: [
+    DatePipe,
     RouterLink
   ],
   templateUrl: './card-cookbook.component.html',
